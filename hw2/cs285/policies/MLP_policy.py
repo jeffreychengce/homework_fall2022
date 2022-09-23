07cc7a49a547747f2b1c7d64d5fc200bd0d00ea2
jeffreychengce <jeffreycheng@berkeley.edu>
2022-09-22 17:00:04 -0700

    Hw2: update mlp policy get action
    from hw 1

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -86,7 +86,18 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from HW1
+        # get this from HW1
+        #!!!
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        # return the action that the policy prescribes
+        observation = ptu.from_numpy(observation.astype(np.float32))
+        action = self(observation)
+        return ptu.to_numpy(action.rsample())
+        #!!!
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
